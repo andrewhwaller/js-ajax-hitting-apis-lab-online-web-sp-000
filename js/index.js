c128542e4dd3c0c00e63ae3887f620a398728afd
Andrew Waller <andrewhwaller@gmail.com>
2019-09-12 05:45:37 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -38,3 +38,20 @@ function getCommits(el) {
   xhr.open('GET', uri);
   xhr.send();
 }
+
+function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><h3>' +
+        commit.commit.author.name +
+        ' (' +
+        commit.author.login +
+        ')</h3>' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
+}
