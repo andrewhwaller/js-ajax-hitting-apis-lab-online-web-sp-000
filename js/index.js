0700dc4fb54b5b34a5be441fc25ab244aeba2c5d
Andrew Waller <andrewhwaller@gmail.com>
2019-09-12 05:35:21 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-// your code here
+const rootURL = 'https://api.github.com';
+
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
