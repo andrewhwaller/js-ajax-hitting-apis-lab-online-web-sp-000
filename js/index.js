8d6dcd1e42d5f68cc74fd1e48cec03a1f39dfbc9
Andrew Waller <andrewhwaller@gmail.com>
2019-09-12 05:46:27 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const rootURL = 'https://api.github.com';
 
 function getRepositories() {
   const name = document.getElementById('username').value;
-  const req = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
   xhr.addEventListener('load', showRepositories);
   xhr.open('GET', `https://api.github.com/users/${username}/repos`);
   xhr.send();
