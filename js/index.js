9f2e60b33a755ccb59daf7c12b98cf993c75486b
Andrew Waller <andrewhwaller@gmail.com>
2019-09-12 05:36:17 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 const rootURL = 'https://api.github.com';
 
 function getRepositories() {
+  const name = document.getElementById('username').value;
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
-  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.open('GET', `https://api.github.com/users/${username}/repos`);
   req.send();
 }
